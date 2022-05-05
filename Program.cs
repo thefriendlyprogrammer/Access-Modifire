d67fbc79065a0b975dfc85c03f47d96232463d70
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-05-05 17:42:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Access_modifire
+{
+    public class Program
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Hi I am Public Access Modifire: \n");
+            //Console.ReadLine();
+        }
+
+        private void Test2()
+        {
+            Console.WriteLine("Hi I am Private Access Modifire: \n");
+        }
+
+        internal void Test3()
+        {
+            Console.WriteLine("Hi I am Internal Access Modifire: \n");
+        }
+
+        protected void Test4()
+        {
+            Console.WriteLine("Hi I am Protected Access Modifire: \n");
+        }
+
+        protected internal void Test5()
+        {
+            Console.WriteLine("Hi I am Protected internal Access Modifire: \n");
+        }
+
+        static void Main(string[] args)
+        {
+            //Program obj = new Program();
+
+            //obj.Test1();
+            //obj.Test2();
+            //obj.Test3();
+            //obj.Test4();
+            //obj.Test5();
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
